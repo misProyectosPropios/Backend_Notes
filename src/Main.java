062301f3751a66e959a9a5060a8b5bb9a4353042
misProyectosPropios <inakifrutos@outlook.com>
2024-09-19 01:00:18 +0200

    ADD features to Note.java. ADD new variables to class: author, ID as finals

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Note nota = new Note("Vamos racing");
+        Note nota = new Note("Vamos racing", 1, "1");
         System.out.println(nota);
         System.out.println("Hello world!");
     }
--- src/Note.java
+++ src/Note.java
@@ -1,19 +1,24 @@
 public class Note {
+    final private int ID;
+    final private String author;
     private String tittle;
     private String body;
 
 
-    public Note() {
-        this("", "");
+    public Note(int ID, String author) {
+        this("", "", ID, author);
     }
 
-    public Note(String body) {
-        this("", body);
+    public Note(String body, int ID, String author) {
+        this("", body, ID, author);
     }
 
-    public Note (String title, String body) {
+    // requires: (forall i : Z) (0 <= i < |title| ==> s[i] != '\n'
+    public Note (String title, String body, int ID, String author) {
         this.tittle = title;
         this.body = body;
+        this.ID = ID;
+        this.author = author;
     }
 
     public void setBody(String body) {
@@ -30,6 +35,14 @@ public class Note {
 
     public String getTittle() {
         return this.tittle;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getID() {
+        return this.ID;
     }
 
     @Override
