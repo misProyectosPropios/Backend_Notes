04d4724c57cdbab83c6c25ec55bc24857a440b57
misProyectosPropios <inakifrutos@outlook.com>
2024-09-22 01:46:31 +0200

    ADD datastructure as the way to create a new connection

--- src/BBDD.java
+++ src/BBDD.java
@@ -1,47 +1,118 @@
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class BBDD {
+    final private int port;
     final private String database_name;
-    final private String table;
+    private Connection con;
+    final private String username;
+    final private String password;
+    final private String server_name;
+    private MysqlDataSource  dataSource = new MysqlDataSource();
+    //f
+    //final private String database_name;
+    //final private String table;
+    //final
 
-    public BBDD(String database_name, String table) {
+    public BBDD(int port, String database_name, String username, String password, String server_name) {
+        this.port = port;
         this.database_name = database_name;
-        this.table = table;
+        this.con = null;
+        this.username = username;
+        this.password = password;
+        this.server_name = server_name;
+        this.dataSource.setPassword(password);
+        this.dataSource.setUser(username);
+        this.dataSource.setServerName(server_name);
+        this.dataSource.setDatabaseName(database_name);
+    }
+
+    public void createConnection() throws SQLException{
+        if (this.dataSource != null) {
+            this.con = this.dataSource.getConnection();
+        }
+    }
+
+    public void closeConnection() throws SQLException {
+        this.con.close();
+        this.con = null;
     }
 
     //Requires (forall i :str) (0 <= i < |columnas| ==> i in this.table)
     //Asures: shows all the columns in the table with the given name in table.
-    public void show_table(String[] columnas) {
-            try {
-            //Creating the connection
-            Connection my_conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.database_name, "root", "");
+    public void show_table(String[] columnas) throws  SQLException {
 
-            //Create obj statement
+        //Create obj statement
+        Statement statement = this.con.createStatement();
 
-            Statement statement = my_conexion.createStatement();
+        //Execute SQL
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCTOS");
 
-            //Execute SQL
+        //Run over the resultSet
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM " + this.table);
-
-            //Run over the resultSet
-
-            while(resultSet.next()) {
-                for(String string : columnas) {
-                    System.out.println(resultSet.getString(string));
-                }
+        while (resultSet.next()) {
+            for (String string : columnas) {
+                System.out.print(resultSet.getString(string));
             }
-            resultSet.close();
+            System.out.println();
         }
-        catch (Exception e) {
-            System.err.println("Exception" + e.getMessage());
-        }
-        System.out.println();
+        resultSet.close();
     }
 
-    public void create_table() {
+    public void create_table(String table_name) throws  SQLException{
+        Statement statement = this.con.createStatement();
+        String query = """ 
+                    CREATE TABLE """ + " IF NOT EXISTS " + table_name + "(" + """
+                    
+                    id int NOT NULL,
+                    author varchar(30) NOT NULL,
+                    title varchar(120) NOT NULL,
+                    body varchar(300) NOT NULL,
+                    last_date DATE NOT NULL,
+                    PRIMARY KEY (ID)
+                );""";
+        statement.executeUpdate(query);
+    }
+
+    public void change_table(String table_name, int id, Note nota_a_guardar) {
+
+    }
+    /*
+    public void create_table(String table_name) throws  SQLException{
+        //Creating the connection
+        Connection my_conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.database_name, "root", "");
+
+        //Create obj statement
+
+        Statement statement = my_conexion.createStatement();
+
+        //Execute SQL
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCTOS");
+
+        //Run over the resultSet
+
+        resultSet.close();
 
     }
 
-    public void change_table() {}
+    public void change_table() throws  SQLException {
+        //Creating the connection
+        Connection my_conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.database_name, "root", "");
+
+        //Create obj statement
+
+        Statement statement = my_conexion.createStatement();
+
+        //Execute SQL
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCTOS");
+
+        //Run over the resultSet
+        resultSet.close();
+    }
+
+     */
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import java.sql.SQLException;
 
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hola");
+        BBDD base_de_datos = new BBDD(3306, "notes", "root", "", "localhost");
+        String[] array = new String[] {"id"};
+        base_de_datos.createConnection();
+        base_de_datos.create_table("veamos");
+        base_de_datos.closeConnection();
     }
 }
